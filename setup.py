c2df7a701ccc9cb29a294700201cd4cc62889dbc
Константин Кожин <kojin@pressindex.ru>
2017-12-05 20:33:07 +0500

    for wheel

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='InstagramLib',
+    version='2017.12.1',
+    author='OlegYurchik',
+    packages=find_packages(),
+    url='https://github.com/OlegYurchik/InstagramLib',
+    classifiers=(
+        'Environment :: Console',
+        'Operating System :: POSIX',
+        'Operating System :: Unix',
+        'Programming Language :: Python :: 3.6',
+    ),
+    install_requires=['requests'],
+    extras_require={
+        'dev': [],
+    },
+)
